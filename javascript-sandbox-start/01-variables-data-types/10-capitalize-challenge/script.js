88e67d6720ac4be53fbe42dd2a04a7eae76e8a02
Ryan <ryan.venturino@gmail.com>
2023-05-27 13:08:27 -0400

    remove extra repo from traversy

--- javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
+++ javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,4 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+let newString = `${myString.charAt(0).toUpperCase() + myString.slice(1, myString.length)}`
+console.log(newString);
