3df8cc8ad0823044b29bdad38c97e41c845ed60b
Ryan Venturino <adventurino@outlook.com>
2023-07-17 19:33:30 -0400

    Finished fetch start flixx app

--- javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -40,9 +40,42 @@ const createTodo = (e) => {
     .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
 
 init();
