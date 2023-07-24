72e69d48862b0dccc2fe8a6a0269c6097e1ea3e0
rventurino <adventurino@outlook.com>
2023-07-24 14:33:46 -0400

    req animationFrames

---
+++ javascript-sandbox-start/12-web-browser-apis/03-canvas/script.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+
+//rectangle
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+
+//draw circle
+ctx.fillStyle = 'red';
+ctx.arc(300, 300, 100, 0, Math.PI * 2);
+ctx.fill();
+
+//draw lines
+ctx.beginPath();
+ctx.strokeStyle = 'orange';
+ctx.lineWidth = 5;
+ctx.moveTo(10, 10);
+ctx.lineTo(300, 300);
+ctx.stroke();
+
+//draw text
+ctx.font = '30px Arial';
+ctx.lineWidth = 1;
+ctx.fillStyle = 'blue';
+//ctx.fillText('Hello World', 300, 100, 300)
+ctx.strokeStyle = 'blue';
+ctx.strokeText('Hello World', 300, 100, 300);
+
+//draw image
+const image = document.querySelector('img');
+image.style.display = 'none';
+image.addEventListener('load', () => ctx.drawImage(image, 270, 270, 100, 100))
---
+++ javascript-sandbox-start/12-web-browser-apis/04-requestAnimationFrame/script.js
@@ -0,0 +1,21 @@
+const image = document.querySelector('img')
+let start;
+let done = false;
+
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+  const elapsed = timestamp - start;
+  console.log(elapsed);
+  if (elapsed > 5000) {
+    done = true;
+  }
+  if (done) {
+    return;
+  }
+  image.style.transform = `translateX(${elapsed/20}px) rotate(${elapsed/20}deg)`
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
