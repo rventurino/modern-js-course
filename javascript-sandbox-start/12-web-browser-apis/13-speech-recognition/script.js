b6ecdddba83933fb5f6cee4a92b39df1f9e4f8f0
rventurino <adventurino@outlook.com>
2023-07-27 17:14:32 -0400

    OOP p1

--- javascript-sandbox-start/12-web-browser-apis/13-speech-recognition/script.js
+++ javascript-sandbox-start/12-web-browser-apis/13-speech-recognition/script.js
@@ -3,7 +3,38 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const rec = new SpeechRecognition();
 
 rec.lang = 'en-US'
-rec.continuous = false;
+rec.continuous = true;
+
+rec.onresult = function (e) {
+    const acceptedColors = [
+      'red',
+      'blue',
+      'green',
+      'yellow',
+      'pink',
+      'brown',
+      'purple',
+      'orange',
+      'black',
+      'white', 
+    ];
+  
+    for (let i = e.resultIndex; i < e.results.length; i++) {
+      const script = e.results[i][0].transcript.toLowerCase().trim();
+  
+      if (acceptedColors.includes(script)) {
+        console.log(script);
+        document.body.style.backgroundImage = '';
+        document.body.style.backgroundColor = script;
+      } else if(script == "cut my life into pieces"){
+        console.log(script);
+        let newBackgroundImage = "url('cutmylifeintopieces.jpg')";
+        document.body.style.backgroundImage = newBackgroundImage;
+      } else {
+        alert(`You said "${script}", please say a color.`);
+      }
+    }
+  };
 
 rec.start();
-//4:06
+
